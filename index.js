67cbca6e387214166a2d46b4f9a7737733aecd1f
Alejandro <jandromix@hotmail.com>
2019-05-12 21:22:43 +0200

    jh

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ const task = require('./entities/tasks');
 const project = require('./entities/projects');
 const state = require('./entities/states');
 var jwt = require('jsonwebtoken')
-const redis = require('redis');
+//const redis = require('redis');
+var Redis = require('ioredis');
+var url   = require('url');
+redis_uri = url.parse(process.env.REDIS_URL);
+
+var redis = new Redis({
+  port: Number(redis_uri.port) + 1,
+  host: redis_uri.hostname,
+  password: redis_uri.auth.split(':')[1],
+  db: 0,
+  tls: {
+    rejectUnauthorized: false,
+    requestCert: true,
+    agent: false
+  }
+});
+
 const { RateLimiterRedis } = require('rate-limiter-flexible');
 const redisClient = redis.createClient({
   enable_offline_queue: false,
