f548d7a569004a77558f19a2b402911b5439e5e4
Alejandro <jandromix@hotmail.com>
2019-05-12 22:10:13 +0200

    jh

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
   console.log("He pasado por redis to go");
   console.log(rtg);
-  var redis = require("redis").createClient(rtg.port, rtg.hostname);
-  
+  var redis = require("redis");
+  var redisClient = redis.createClient(rtg.port, rtg.hostname);
+  console.log(redis);
   redis.auth(rtg.auth.split(":")[1]);
 } else {
   var redis = require("redis");
